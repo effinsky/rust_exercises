8a6d3d92b7ed90a75578e8c3ca6c19b45df03da1
adi forkasiewicz <adi.forkasiewicz@ingrid.com>
2024-04-08 14:07:06 +0200

    fixes to permutation

--- src/exercises/permutation.rs
+++ src/exercises/permutation.rs
@@ -6,7 +6,7 @@ pub fn permutate<A: Clone>(l: &[A]) -> Vec<A> {
     }
 
     let len = l.len();
-    let s = random_vec(0, len);
+    let s = shuffle(0, len);
 
     s.into_iter()
         .fold(Vec::with_capacity(l.len()), |mut acc, idx| {
@@ -16,18 +16,18 @@ pub fn permutate<A: Clone>(l: &[A]) -> Vec<A> {
 }
 
 // Durstenfeld's version of the Fisher-Yates shuffle.
-fn random_vec(low: usize, high: usize) -> Vec<usize> {
+fn shuffle(low: usize, high: usize) -> Vec<usize> {
     let mut rng = rand::thread_rng();
-    let mut values = (low..high).collect::<Vec<usize>>();
+    let mut out = (low..high).collect::<Vec<usize>>();
 
     for idx in low..high {
         // get random index
         let new_idx = rng.gen_range(idx..high);
         // swap the item under that index with the last element
         let last_untouched_idx = high - idx - 1;
-        values.swap(new_idx, last_untouched_idx);
+        out.swap(new_idx, last_untouched_idx);
         // and then that index has a value already used and is out of bounds
     }
 
-    values
+    out
 }
