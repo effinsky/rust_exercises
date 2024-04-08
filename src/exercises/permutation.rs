43cca915f6419d3738b8a4b3695c1ec1f711f726
adi forkasiewicz <adi.forkasiewicz@ingrid.com>
2024-04-08 13:42:14 +0200

    fixes to permutation

--- src/exercises/permutation.rs
+++ src/exercises/permutation.rs
@@ -19,13 +19,12 @@ pub fn permutate<A: Clone>(l: &[A]) -> Vec<A> {
 fn random_vec(low: usize, high: usize) -> Vec<usize> {
     let mut rng = rand::thread_rng();
     let mut values = (low..high).collect::<Vec<usize>>();
-    let len = values.len();
 
-    for idx in 0..values.len() {
+    for idx in low..high {
         // get random index
-        let new_idx = rng.gen_range(idx..values.len());
+        let new_idx = rng.gen_range(idx..high);
         // swap the item under that index with the last element
-        let last_untouched_idx = len - idx - 1;
+        let last_untouched_idx = high - idx - 1;
         values.swap(new_idx, last_untouched_idx);
         // and then that index has a value already used and is out of bounds
     }
